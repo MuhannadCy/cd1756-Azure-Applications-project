a818ce8ed8822d3fef7b0e1c857287a4ee595599
MuhannadCy <muhannad.h.alanazi@gmail.com>
2022-10-09 23:54:13 +0300

    plz work

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -15,25 +15,6 @@ app.logger.setLevel(logging.WARNING)
 streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.WARNING)
 app.logger.addHandler(streamHandler)
-
-app.errorhandler(NotFound)
-def page_not_found_handler(e: HTTPException):
-    return render_template('404.html'), 404
-
-
-app.errorhandler(Unauthorized)
-def unauthorized_handler(e: HTTPException):
-    return render_template('401.html'), 401
-
-
-app.errorhandler(Forbidden)
-def forbidden_handler(e: HTTPException):
-    return render_template('403.html'), 403
-
-
-app.errorhandler(RequestTimeout)
-def request_timeout_handler(e: HTTPException):
-    return render_template('408.html'), 408
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
